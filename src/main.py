08ce95ecffcab9a563e747f0b51511221dbd8248
Talha Rashid <T.rashid489@gmail.com>
2022-12-17 03:03:45 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import discord
 import os
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
 
-client = discord.Client(intents=discord.Intents.default())
+
+client = discord.Client(command_prefix='#', intents=discord.Intents().all())
 
 @client.event
 async def on_ready():
     print('MyTime Bot active and running as {0.user}'.format(client))
 
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('DISCORD_TOKEN'))
