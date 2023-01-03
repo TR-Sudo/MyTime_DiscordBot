f77f96e9e394733b764c45784ef223e7776a43a8
Talha Rashid <T.rashid489@gmail.com>
2023-01-02 23:19:29 -0500

    added guide for each command

--- src/main.py
+++ src/main.py
@@ -17,13 +17,16 @@ async def on_ready():
     print('MyTime Bot active and running as {0.user}'.format(bot))
 
 
-@bot.command(name='Setup',help="Guide to using this bot\nThere are two steps to set up and use MyTime:\n\n1.Set your default timezone by typing {>MyLocation [enter timezone]}\n2.Convert the time in one time zone to your time zone [>MyTime [time (e.g. 5:00pm)] [Timezone (e.g. EST)]}\n\nWithout setting a default timezone\n3.{#TimeTo [Type >help TimeTo for description]}\n4.{#TzToTz [Type >help TzToTz for description]}")
+@bot.command(name='Setup',help="Guide to using this bot")
 async def sendHelp(ctx):
     if ctx.message.author==bot.user:
         return 
-    await ctx.send()
+    if ctx.message==">":
+        return
+    await ctx.send("There are two steps to set up and use MyTime:\n\n1.Set your default time zone by typing {>MyLocation [enter time zone]}\n2.Convert the time in one time zone to your time zone [>MyTime [time (e.g. 5:00pm)] [Time zone (e.g. EST)]}\n\nWithout setting a default time zone\n3.>TimeTo [Type >help TimeTo] for description\n4.>TzToTz [Type >help TzToTz] for description")
 
-@bot.command(name='TimeTo',help="Convert Exact Time\nInput [time in 12hr]/[Year]/[Month]/[Day] [Previous Timezone] [Requested Timezone]\nEX->12:30pm/2023/02/03 EST PST")
+#Converts exact time with year month and day to requested time zone, no need for user setup
+@bot.command(name='TimeTo',help="Convert Exact Time\nInput [time in 12hr]/[Year]/[Month]/[Day] [Previous Time zone] [Requested Time zone]\nEX->12:30pm/2023/02/03 EST PST")
 async def converTime(ctx,prev_time: str = commands.parameter(description="[time in 12hr]/[Year]/[Month]/[Day]"),prev_tz: str=commands.parameter(description="Previous time zone"),time_zone: str =commands.parameter(description="to time zone")):
     unixTime=(conToUnixTime(prev_time))
     prev_tz=prev_tz.upper()
@@ -34,8 +37,9 @@ async def converTime(ctx,prev_time: str = commands.parameter(description="[time 
     else:
         await ctx.send(INC_TIME_ZONE)
 
-@bot.command(name="TzToTz",help="Convert 12 Hour time to different time zones\n[12 Hour Time] [Previous Time Zone] [New Time Zone]\nEX-> 12:00PM EST PST")
-async def conver12Time(ctx,prevTime: str = commands.parameter(description="[12 Hour Time] -> EX.12:30PM"),prev_tz: str=commands.parameter(description="Previous time zone"),time_zone: str =commands.parameter(description="to time zone")):
+#Converts time from one zone to another no need for user setup
+@bot.command(name="TzToTz",help="Convert 12 Hour time\n[12 Hour Time] [Previous Time Zone] [New Time Zone]\nEX-> 12:00PM EST PST")
+async def conver12Time(ctx,prevTime: str = commands.parameter(description="[12 Hour Time]  EX->12:30PM"),prev_tz: str=commands.parameter(description="Previous time zone"),time_zone: str =commands.parameter(description="to time zone")):
     unixTime=(con12TimeToUnix(prevTime))
     prev_tz=prev_tz.upper()
     time_zone=time_zone.upper()    
@@ -45,8 +49,9 @@ async def conver12Time(ctx,prevTime: str = commands.parameter(description="[12 H
     else:
         await ctx.send(INC_TIME_ZONE)
 
-@bot.command(name="MyLocation",help="Set your default timezone by typing {>MyLocation [enter timezone]}")
-async def setTz(ctx,time_zone):
+#Adds user to database with his requested time zone
+@bot.command(name="MyLocation",help="Set your default timezone by typing {>MyLocation [enter time zone]}")
+async def setTz(ctx,time_zone: str = commands.parameter(description="[Time Zone] Ex-> EST")):
     time_zone=time_zone.upper()
 
     if(check_time_zone(time_zone)):
@@ -54,8 +59,9 @@ async def setTz(ctx,time_zone):
     else:
         await ctx.send(INC_TIME_ZONE)
 
-@bot.command(name="MyTime",help="Convert the time in one time zone to your time zone [>MyTime [time (e.g. 5:00pm)] [Timezone (e.g. EST)]}")
-async def get_tz(ctx,prev_time,time_zone):
+#Converts time to users time zone 
+@bot.command(name="MyTime",help="Convert the time in one time zone to your time zone\n[>MyTime [time (e.g. 5:00pm)] [Timezone (e.g. EST)]}")
+async def get_tz(ctx,prev_time:str = commands.parameter(description="[12 Hour Time]  EX->12:30PM"),time_zone: str = commands.parameter(description="[Time Zone] Ex-> EST")):
     unixTime=(con12TimeToUnix(prev_time))
     time_zone=time_zone.upper()
     
@@ -68,5 +74,4 @@ async def get_tz(ctx,prev_time,time_zone):
     else:
         await ctx.send(INC_TIME_ZONE)
 
-#Todo: add MyTime by getting users time zone value
 bot.run(os.getenv('DISCORD_TOKEN'))
